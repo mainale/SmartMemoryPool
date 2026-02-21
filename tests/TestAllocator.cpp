1b56c1258f58a75eb611405a56609fe4876da575
mainale <3066573506@qq.com>
2026-02-21 16:44:50 +0800

    Add simple test file

--- tests/TestAllocator.cpp
+++ tests/TestAllocator.cpp
@@ -1,11 +1,17 @@
 //
 // Created by 30665 on 26-2-21.
 //
-#include "SizeClassMemoryPool.h"
-#include "MemoryPoolAllocator.h"
+// tests/TestAllocator.cpp
 #include <iostream>
+#include "MemoryPoolAllocator.h"
+#include "SizeClassMemoryPool.h"
 
 int main() {
-    std::cout << "Test placeholder – replace with actual tests.\n";
+    std::cout << "Running tests...\n";
+    SizeClassMemoryPool pool(100);
+    MemoryPoolAllocator<int> alloc(pool);
+    int* p = alloc.allocate(10);
+    alloc.deallocate(p, 10);
+    std::cout << "All tests passed!\n";
     return 0;
 }
